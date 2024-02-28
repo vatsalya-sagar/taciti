bbcdb54eecf1089ae88fca1bfe58436ab3e70b38
sagar shirsat <sagar@cloudxperte.com>
2024-02-28 18:55:22 +0530

    Footer and Global.css file working

--- component/Footer.js
+++ component/Footer.js
@@ -4,128 +4,91 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      {/* <div id="Contact">
+      <div id="footer" className="footer">
         <div className="footerBg">
           <div className="innerCard">
-            <div className="box">
-              <h1>
-                Suite #204 <br />
-                6010 W Spring Creek Pkwy, <br />
-                Plano, TX 75024, USA <br />{" "}
-              </h1>
-              <h2>info@taciti.com</h2>
-            </div>
-            <div className="servicesBox">
-              <h1 className="serTxt">Services</h1>
-              <p>Strategy & Advisory</p>
-              <p>ERP Lead Transformation</p>
-              <p>Transformation Management & Goverance</p>
-              <p>Technology Services</p>
-              <p>Human Capital Management</p>
-            </div>
-            <div className="servicesBox">
-              <h1 className="serTxt">Services</h1>
-              <p>Strategy & Advisory</p>
-              <p>ERP Lead Transformation</p>
-              <p>Transformation Management & Goverance</p>
-              <p>Technology Services</p>
-              <p>Human Capital Management</p>
-            </div>
-            <div className="servicesBox">
-              <h1 className="serTxt">Services</h1>
-              <p>Strategy & Advisory</p>
-              <p>ERP Lead Transformation</p>
-              <p>Transformation Management & Goverance</p>
-              <p>Technology Services</p>
-              <p>Human Capital Management</p>
-            </div>
-          </div>
-        </div>
-      </div> */}
-      <div id="footer">
-        <div className="footerBg">
-          <div className="innerCard   col-12 ">
             <div className="row">
-              <div className="col-md-3">
-                <div className="box">
-                  <h1>
-                    Suite #204 <br />
-                    6010 W Spring Creek Pkwy, <br />
-                    Plano, TX 75024, USA <br />
-                  </h1>
-                  <h2>info@taciti.com</h2>
-                </div>
+              <div className="box addressBox">
+                <h1 className="serTxt">
+                  Suite #204 <br />
+                  6010 W Spring Creek Pkwy, <br />
+                  Plano, TX 75024, USA <br />
+                </h1>
+                <h2 className="ch2">info@taciti.com</h2>
               </div>
-              <div className="col-md-3">
-                <div className="servicesBox">
-                  <h1 className="serTxt">Services</h1>
-                  <p>
-                    <Link href="/business-consulting/" className="custom-link">
-                      Strategy & Advisory
-                    </Link>
-                  </p>
-                  <p>
-                    <Link href="/process-advisory/">
-                      ERP Lead Transformation
-                    </Link>
-                  </p>
-                  <p>
-                    {" "}
-                    <Link href="/business-transformation/">
-                      Transformation Management & Goverance
-                    </Link>
-                  </p>
-                  <p>
-                    <Link href="/technology-services/">
-                      Technology Services
-                    </Link>
-                  </p>
-                  <p>
-                    {" "}
-                    <Link href="/talent-management-transformation/">
-                      Human Capital Management
-                    </Link>
-                  </p>
-                </div>
+              <div className="servicesBox">
+                <h1 className="serTxt">Services</h1>
+                <p>
+                  <Link href="/business-consulting/" className="customLink">
+                    Strategy & Advisory
+                  </Link>
+                </p>
+                <p>
+                  <Link href="/process-advisory/" className="customLink ">
+                    ERP Lead Transformation
+                  </Link>
+                </p>
+                <p>
+                  {" "}
+                  <Link href="/business-transformation/" className="customLink">
+                    Transformation Management <br></br>& Governance
+                  </Link>
+                </p>
+                <p>
+                  <Link href="/technology-services/" className="customLink ">
+                    Technology Services
+                  </Link>
+                </p>
+                <p>
+                  {" "}
+                  <Link
+                    href="/talent-management-transformation/ "
+                    className="customLink"
+                  >
+                    Human Capital Management
+                  </Link>
+                </p>
               </div>
-              <div className="col-md-3">
-                <div className="servicesBox">
-                  <h1 className="serTxt">Industries</h1>
-                  <p>
-                    <Link
-                      href="/manufacturing-industries/"
-                      className="custom-link"
-                    >
-                      Manufacturing Industries{" "}
-                    </Link>
-                  </p>
-                  <p>
-                    <Link href="/life-sciences/">Life Sciences</Link>
-                  </p>
-                  <p>
-                    {" "}
-                    <Link href="/consumer-packaged-goods/">
-                      Consumer Packaged Goods{" "}
-                    </Link>
-                  </p>
-                  <p>
-                    <Link href="/hi-tech/">Hi-Tech </Link>
-                  </p>
-                  <p>
-                    {" "}
-                    <Link href="/utility/">Utilities</Link>
-                  </p>
-                </div>
+              <div className="servicesBox">
+                <h1 className="serTxt">Industries</h1>
+                <p>
+                  <Link
+                    href="/manufacturing-industries/"
+                    className="customLink"
+                  >
+                    Manufacturing Industries{" "}
+                  </Link>
+                </p>
+                <p>
+                  <Link href="/life-sciences/" className="customLink">
+                    Life Sciences
+                  </Link>
+                </p>
+                <p>
+                  {" "}
+                  <Link href="/consumer-packaged-goods/" className="customLink">
+                    Consumer Packaged Goods{" "}
+                  </Link>
+                </p>
+                <p>
+                  <Link href="/hi-tech/" className="customLink">
+                    Hi-Tech{" "}
+                  </Link>
+                </p>
+                <p>
+                  {" "}
+                  <Link href="/utility/" className="customLink">
+                    Utilities
+                  </Link>
+                </p>
               </div>
-              <div className="col-md-3 ">
-                <div className="servicesBox">
-                  <h1 className="serTxt">Qulck Links</h1>
-                  <p>
-                    <Link href="/contact-us/" className="custom-link">
-                      Contact Us
-                    </Link>
-                  </p>
-                </div>
+              <div className="servicesBox">
+                <h1 className="serTxt">Quick Links</h1>
+                <p>
+                  <Link href="/contact-us/" className="customLink">
+                    Contact Us
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
